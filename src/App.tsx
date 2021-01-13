a3fe2c74a0f7766899ece7c1bc39bd46f5203ff3
sunman7 <473910527@qq.com>
2021-01-13 16:48:02 +0800

    init Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./components/Nav";
+
+const Wrapper = styled.div` 
+ border:1px solid red;
+ min-height: 100vh;
+ display: flex;
+ flex-direction: column;
+ 
+ 
+`;
+const Main = styled.div`
+ border: 1px solid green;
+ flex-grow: 1;
+ overflow: auto;
+`;
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                        <Route exact path="/tags">
+                            <Tags/>
+                        </Route>
+                        <Route exact path="/record">
+                            <Records/>
+                        </Route>
+                        <Route exact path="/statistics">
+                            <Statistics/>
+                        </Route>
+
+                        <Redirect exact from="/" to="/record"/>
+                        <Route exact path="*">
+                            <NoMatch/>
+                        </Route>
+                    </Switch>
+                </Main>
+                <Nav />
+            </Wrapper>
+        </Router>
+    );
+}
+
+function Statistics() {
+    return <h2>统计</h2>;
+}
+
+function Tags() {
+    return <h2>标签</h2>;
+}
+
+function Records() {
+    return <h2>记账</h2>;
+}
+
+function NoMatch() {
+    return (
+        <div>地址有误</div>
+    );
+
 }
 
 export default App;
