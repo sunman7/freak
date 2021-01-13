73d388cc563fe74c2efff919c2e3702b22dd2611
sunman7 <473910527@qq.com>
2021-01-13 17:24:53 +0800

    添加SVG图片 完成Nav样式

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
+require("../icons/record.svg");
+require("../icons/tag.svg");
+require("../icons/stat.svg");
 
 const NavWrapper = styled.nav`
 line-height: 24px;
@@ -10,7 +13,11 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
   >li{
      width:33.33%;
      text-align: center;
-     padding: 16px;
+     padding: 4px 0;
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
   }
 }
 `;
@@ -19,12 +26,21 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#tag" />
+                    </svg>
                     <Link to="/tags">标签页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#record" />
+                    </svg>
                     <Link to="/record">记账</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#stat" />
+                    </svg>
                     <Link to="/statistics">统计</Link>
                 </li>
             </ul>
